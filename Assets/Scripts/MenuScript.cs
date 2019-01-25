fcbbe7ad98b3a2b001334b907f5247c769b057cf
PeterJamesArmstrong <ashen_s@yahoo.com.au>
2019-01-26 09:27:13 +1100

    Basic Splash Scene and Menu Scene created

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.IO;
+
+public class MenuScript : MonoBehaviour {
+
+    [SerializeField]
+    private string GameSceneName = "GameScene";
+
+
+	private void Start ()
+	{
+
+	}
+
+	public void StartGame()
+    {
+
+        SceneManager.LoadScene(GameSceneName);
+
+    }
+
+
+
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+
+    
+
+
+}
---
+++ Assets/Scripts/SplashManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashManager : MonoBehaviour {
+	private float _ftimer;
+
+	[SerializeField]
+	private float _timeLimit;
+
+    [SerializeField]
+        private string MenuSceneName;
+
+	// Use this for initialization
+	void Start () {
+		_ftimer = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_ftimer += Time.deltaTime;
+
+		if (_ftimer > _timeLimit)
+			SceneManager.LoadScene(MenuSceneName);
+
+	}
+}
