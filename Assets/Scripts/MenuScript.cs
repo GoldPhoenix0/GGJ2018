e081a60d45fcced6aa4a535e4b7249746dc8c2f6
PeterJamesArmstrong <ashen_s@yahoo.com.au>
2019-01-26 10:25:21 +1100

    Created a persistentData singleton to track number of players and whatever else we want to move between the menu scene and the game scene. Updated GGJ splash texture

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -10,10 +10,26 @@ public class MenuScript : MonoBehaviour {
     [SerializeField]
     private string GameSceneName = "GameScene";
 
+    [SerializeField]
+    private Text NumberOfPlayersUI;
 
-	private void Start ()
+
+    public void ChangeNumberOfPlayers(int deltaNOP)
+    {
+        PersistentData.instance.NumberOfPlayers += deltaNOP;
+
+        if (PersistentData.instance.NumberOfPlayers > PersistentData.instance.MaxNumberOfPlayers)
+            PersistentData.instance.NumberOfPlayers = 2;
+
+        if (PersistentData.instance.NumberOfPlayers == 1)
+            PersistentData.instance.NumberOfPlayers = PersistentData.instance.MaxNumberOfPlayers;
+
+        // update UI
+        NumberOfPlayersUI.text = PersistentData.instance.NumberOfPlayers.ToString();
+    }
+
+    private void Start ()
 	{
-
 	}
 
 	public void StartGame()
---
+++ Assets/Scripts/PersistentData.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersistentData : MonoBehaviour
+{
+    public int NumberOfPlayers = 2;
+    public int MaxNumberOfPlayers = 4;
+
+    public static PersistentData _instance;
+
+
+    public static PersistentData instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                GameObject tmpObj = new GameObject();
+                tmpObj.name = "PersistentObj";
+                _instance = tmpObj.gameObject.AddComponent<PersistentData>();
+            }
+            return _instance;
+        }
+    }
+
+
+
+    // Use this for initialization
+    void Start () {
+        if (_instance != null && this != PersistentData.instance  )
+            Destroy(this);
+        else
+        {
+            _instance = this;
+            DontDestroyOnLoad(this);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
