c8f380379528250a3998e150c14cfe38ec1d187f
PeterJamesArmstrong <ashen_s@yahoo.com.au>
2019-01-27 03:38:55 +1100

    Setup a heap of prefabs for the different shaped pieces. Added items for them all to the FurniturePicker scroll view.  Still need to set their id on the button where it handles the onClick event.

---
+++ Assets/Scripts/Piece/ScrollViewControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollViewControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    public void thingy(int FurnitureID)
+    {
+
+        Debug.Log(FurnitureID);
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
