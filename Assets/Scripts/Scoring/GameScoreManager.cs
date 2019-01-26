fdcdf849cf034db77f1b63adc574ae7205476971
PeterJamesArmstrong <ashen_s@yahoo.com.au>
2019-01-26 12:30:45 +1100

    Setting up the scoring UI based on number of players

---
+++ Assets/Scripts/Scoring/GameScoreManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScoreManager : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject scorePanel;
+    [SerializeField]
+    private GameObject scoreDisplayPrefab;
+
+    private List<PlayerScore> Scores;
+
+    // Use this for initialization
+    void Start()
+    {
+        Scores = new List<PlayerScore>();
+
+        for (int i = 0; i < PersistentData.instance.NumberOfPlayers; i++)
+        {
+            GameObject tmpGO;
+            PlayerScore tmpPS;
+            tmpGO = Instantiate(scoreDisplayPrefab, scorePanel.transform);
+
+            tmpPS = tmpGO.GetComponent<PlayerScore>();
+            Scores.Add(tmpPS);
+
+            Scores[i].PlayerNumber = i;
+            Scores[i].SetScorePosition();
+            Scores[i].PlayerScoreValue = 0;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Scoring/PlayerScore.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerScore : MonoBehaviour {
+
+    [SerializeField]
+    private Text playerScoreLabel;
+    [SerializeField]
+    private Text playerScoreField;
+
+    private int _PlayerNumber;
+
+    private int _PlayerScoreValue;
+
+    public int PlayerScoreValue
+    {
+        get
+        {
+            return _PlayerScoreValue;
+        }
+        set
+        {
+            _PlayerScoreValue = value;
+            playerScoreField.text = _PlayerScoreValue.ToString();
+        }
+    }
+
+    public int PlayerNumber
+    {
+        get
+        {
+            return _PlayerNumber;
+        }
+        set
+        {
+            _PlayerNumber = value;
+            playerScoreLabel.text = "Player " + (_PlayerNumber + 1).ToString();  // Since player numbers start from 0 but humans don't
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    public void SetScorePosition()
+    {
+        RectTransform myRT;
+
+        myRT = (RectTransform)this.gameObject.transform;
+        Vector2 vals;
+        vals = myRT.anchorMin;
+        vals.x = PlayerNumber * (1.0f / PersistentData.instance.NumberOfPlayers);
+        myRT.anchorMin = vals;
+
+        vals = myRT.anchorMax;
+        vals.x = (PlayerNumber + 1) * (1.0f / PersistentData.instance.NumberOfPlayers);
+        myRT.anchorMax = vals;
+
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
